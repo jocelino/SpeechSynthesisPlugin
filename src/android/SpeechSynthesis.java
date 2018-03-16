74d79805eb467ddde12f2dfa14ab71a7dce2467e
jocelino <celino.jp11@gmail.com>
2018-03-16 12:43:44 -0300

    Update SpeechSynthesis.java

--- src/android/SpeechSynthesis.java
+++ src/android/SpeechSynthesis.java
@@ -242,10 +242,10 @@ public class SpeechSynthesis extends CordovaPlugin implements OnInitListener, On
     @Override
     public void onRangeStart (String utteranceId, int start, int end, int frame){
         JSONObject event = new JSONObject();
-        error.put("type","boundry");
-        error.put("charIndex",start);
-        error.put("elapsedTime",end);
-        error.put("name",frame);
+        event.put("type","boundry");
+        event.put("charIndex",start);
+        event.put("elapsedTime",end);
+        event.put("name",frame);
         PluginResult pr = new PluginResult(PluginResult.Status.OK, event);
         pr.setKeepCallback(true);
         callbackContext.sendPluginResult(pr);
